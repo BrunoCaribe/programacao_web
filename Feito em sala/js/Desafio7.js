4dcee9a7cf415edc278390e1cc31ac60446dc4e0
BrunoCaribe <44852946+BrunoCaribe@users.noreply.github.com>
2020-03-16 20:00:10 -0300

    Setimo Desafio

--- Feito em sala/js/Desafio7.js
+++ Feito em sala/js/Desafio7.js
@@ -13,3 +13,21 @@ console.log("O segundo elemento do array é " + array[1])
 console.log("O primeiro array tem  " + array.length + " itens")
 
 console.log("O segundo  array tem  " + array[].length + " itens")
+
+var i = 10
+
+while (i <= 20) {
+    i % 2 == 0 ? console.log('Números pares entre 10 e 20: ' + i) : false
+    i++
+}
+var j = 10
+while (j <= 20) {
+    j % 2 != 0 ? console.log('Números ímpares entre 10 e 20: ' + j) : false
+    j++
+}
+for (var i = 100; i <= 120; i++) {
+    i % 2 == 0 ? console.log('Números pares entre 100 e 120:' + i) : false
+}
+for (var j = 111; j <= 125; j++) {
+    j % 2 != 0 ? console.log('Números ímpares entre 111 e 125: ' + j) : false
+}
